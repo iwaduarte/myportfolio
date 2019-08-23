1420cca1b9c4af9231afc3260bc7974e830e0601
iwaduarte <4816534+iwaduarte@users.noreply.github.com>
2019-08-23 18:41:21 +1000

    react-icons and profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from "./app/_common/Main/Header";
 import Main from "./app/_common/Main/Main";
 import Footer from "./app/_common/Main/Footer";
 
 const App = () => <>
-    <Header/>
     <Main/>
     <Footer/>
 </>;
--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import {GoMarkGithub} from 'react-icons/go';
+import {FaStackOverflow, FaLinkedinIn, FaLinkedin} from 'react-icons/fa/index'
 
 const Home = props => <>
-    <div> Hello World </div>
-</>;
+        <div className='home'>
+
+            <div className="profile">
+
+                <div className="profile-head">
+                    <div className="rounded">
+
+                        <img className="logo " src={'/images/profile-picture.png'} alt={'Logo Profile'}/>
+                    </div>
+
+                    <div className="flex">
+                        <div>
+                            Full Stack Developer
+                        </div>
+                        <GoMarkGithub />
+                        <FaStackOverflow/>
+                        <FaLinkedin value={{color:"blue"}}/>
+                        <img className="icon" src={'/images/freelancer-icon.jpeg'} alt={'Logo freelancer'}/>
+
+                        <div>
+                            A full stack developer with experience in several technologies...
+                            I also like to binge watch Netflix.
+                            And talk about meaningful things and cry.
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="moreInfo">
+
+                </div>
+
+            </div>
+        </div>
+    </>
+;
 
 export default Home;
