d38e4855c5550aaae6fd968418d4d82990970a20
iwaduarte <iwaduarte@gmail.com>
2020-03-01 02:50:38 -0300

    fix state behaviour
    - properly set Link and text in footer's interactions

--- src/app/bot/BotFooter.js
+++ src/app/bot/BotFooter.js
@@ -1,33 +1,26 @@
 import React, {useState} from 'react';
 
-const footerStates = ["🤖 Stop, this is serious! We need talk.",
+const footerStates = ["Stop, this is serious! We need talk.",
     `Nice... 😠 Can we talk now?`,
-    `❤ To access the portfolio one must interact with the bot. Otherwise you could try on LinkedIn or GitHub`];
+    `❤ To access the portfolio one must interact with the bot. Otherwise you could try on LinkedIn or GitHub 🤙`];
 const iterator = footerStates[Symbol.iterator]();
-const links = ["https://goodreads.com/quotes/tag/seriousness", "", "https://github.com/iwaduarte"];
+const links = ["https://goodreads.com/quotes/tag/seriousness", null, "https://github.com/iwaduarte"];
 const linkIterator = links[Symbol.iterator]();
 const firstFooterStates = iterator.next().value; // executes only once
-const firstLink = linkIterator.next().value; // executes only once
 
 const BotFooter = props => {
 
-    const [link, setLink] = useState(firstLink);
+    const [link, setLink] = useState(0);
     const [weNeedTalk, setWeNeedTalk] = useState(firstFooterStates);
 
     return <footer className={"bot-footer"}>
         <div className="flex-items m-auto">
             <a onClick={() => {
                 const footerState = iterator.next();
+                !footerState.done && setWeNeedTalk(footerState.value);
+
                 const footerLink = linkIterator.next();
-                console.log('footerState value ', footerState.value);
-
-                if (footerState.done) {
-                    return;
-                }
-
-                setLink(footerLink.value);
-                setWeNeedTalk(footerState.value);
-
+                !footerLink.done && setLink(footerLink.value);
 
             }}
                target={"_blank"} href={link}>{weNeedTalk}</a>
