665662abb5518598f6078a8fc00ae58678d3e0bd
iwaduarte <iwaduarte@gmail.com>
2020-02-29 21:52:12 -0300

    create iterator to go through array

--- src/app/bot/BotFooter.js
+++ src/app/bot/BotFooter.js
@@ -1,12 +1,36 @@
 import React, {useState} from 'react';
 
+const footerStates = ["🤖 Stop, this is serious! We need talk.",
+    `Nice... 😠 Can we talk now?`,
+    `❤ To access the portfolio one must interact with the bot. Otherwise you could try on LinkedIn or GitHub`];
+const iterator = footerStates[Symbol.iterator]();
+const links = ["https://goodreads.com/quotes/tag/seriousness", "", "https://github.com/iwaduarte"];
+const linkIterator = links[Symbol.iterator]();
+const firstFooterStates = iterator.next().value; // executes only once
+const firstLink = linkIterator.next().value; // executes only once
+
 const BotFooter = props => {
-    const [weNeedTalk, setWeNeedTalk] = useState("Stop, this is serious! We need talk");
+
+    const [link, setLink] = useState(firstLink);
+    const [weNeedTalk, setWeNeedTalk] = useState(firstFooterStates);
 
     return <footer className={"bot-footer"}>
         <div className="flex-items m-auto">
-            <a onClick={() => setWeNeedTalk("Nice bot. Can we talk now")}
-               href="https://goodreads.com/quotes/tag/seriousness" target={"blank"}>{weNeedTalk}</a>
+            <a onClick={() => {
+                const footerState = iterator.next();
+                const footerLink = linkIterator.next();
+                console.log('footerState value ', footerState.value);
+
+                if (footerState.done) {
+                    return;
+                }
+
+                setLink(footerLink.value);
+                setWeNeedTalk(footerState.value);
+
+
+            }}
+               target={"_blank"} href={link}>{weNeedTalk}</a>
         </div>
         <hr/>
         <div className="flex-items m-auto">
