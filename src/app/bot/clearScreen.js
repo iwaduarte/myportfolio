512b1e752451c834dc75bb6237266dff2635af9e
iwaduarte <iwaduarte@gmail.com>
2020-03-05 18:43:33 -0300

    Function to clear screen

---
+++ src/app/bot/clearScreen.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from "react";
+
+
+export default function clearScreen() {
+    const clearMessage = <div>
+        <h1 className={"m-auto txt-align-center my-3"}>How about now smart-ass...  </h1>
+        <span role="img" className=" m-auto  txt-align-center d-block smile-emoji" aria-label="smile-emoji">😁</span>
+    </div>;
+
+    return ReactDOM.render(clearMessage, document.getElementById('root'));
+};
