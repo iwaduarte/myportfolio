dfd9cd20fa522f0f85278456d09ab85129d1b31c
iwaduarte <iwaduarte@gmail.com>
2020-03-07 23:16:33 -0300

    Article section for my technical wanders and wisdom

--- src/app/home/Articles.js
+++ src/app/home/Articles.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 
-const Articles = props => <>
+const articles = []; //medium articles or personal blog development which is easier (less hassle)
 
-</>;
+const handleClick = () => {
+    console.log('New article...');
+};
+const Articles = props => {
+    const [article, setArticle] = useState([]);
+
+    return < div className="article flex-container self-center ">
+        <FaChevronLeft onClick={handleClick} size="50" color="rgb(108,103,134)" className="mx-1 pointer scale-hover"/>
+        {articles.length
+            ? articles.map(article => <div> {article}</div>)
+            : <div className="self-center">Articles are being made... Wait a little my friend.
+                <span className="profile-emoji" role="img" aria-label='wink-emoji'> 😉 </span>
+            </div>
+        }
+        <FaChevronRight onClick={handleClick} size="50" color="rgb(108,103,134)" className="mx-1 pointer scale-hover"/>
+    </div>
+
+};
 
 export default Articles;
