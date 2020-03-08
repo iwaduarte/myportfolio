ebbd32110f5fb156bcc7ad65e9a276d1e08f2af5
iwaduarte <iwaduarte@gmail.com>
2020-03-07 23:16:42 -0300

    Contact form added

--- src/app/home/Contact.js
+++ src/app/home/Contact.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 const Contact = props => <>
-    <form>
-        Email: <input type="text"/>
-        Subject: <input type="text"/>
+    <form className="flex-container direction-column space-evenly m-auto profile-form">
+
+        <div className="email ">
+            <label className="small-font-2 bold"> Your Email: </label>
+            <input className="home-input-field" type="text"/>
+        </div>
+        <div className="subject   ">
+           <label className="small-font-2 bold"> Subject: </label>
+            <input className="home-input-field" type="text"/>
+        </div>
+        <div className="body mt-1 mb-0 ">
+         <label className="d-block small-font-2 bold"> Quick message: </label>
+            <textarea className="home-input-field textarea mt-1 mb-0" name="Body" id="" cols="50" rows="3"/>
+        </div>
+        <button className="videos-button mt-0" type="submit "> Submit </button>
 
 
     </form>
+
 </>;
 
 export default Contact;
