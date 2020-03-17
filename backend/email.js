fdcf161c31ea32fd33efc3a244f4e07587e3bf50
iwaduarte <iwaduarte@gmail.com>
2020-03-16 23:24:59 -0300

    Updated email.js & serverless.yml to send messages on POST requests and sanitize user input

--- backend/email.js
+++ backend/email.js
@@ -1,16 +1,41 @@
 'use strict';
+
 const AWS = require('aws-sdk');
-const ses = new AWS.SES({ apiVersion: "2010-12-01", region: "us-east-1"});
+const querystring = require('querystring');
+
+const ses = new AWS.SES({apiVersion: "2010-12-01", region: "us-east-1"});
 const sendTo = process.env.SEND_TO;
+
+
+const htmlEscape = (text) => {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/"/g, '&quot;') // it's not necessary to escape >
+        .replace(/'/g, '&#039;');
+};
+
 
 exports.handler = async event => {
 
-
-    const subject = "Test email"
-    const message = "My message to iwa. Please enter in contact I want to hire you!"
-    const source = "contact@iwaduarte.dev";
-
-    // const {subject, message, source} = event.body;
+    const body = querystring.decode(event.body);
+    const {message, appSource, from, subject} = body;
+    const response = {
+        statusCode: 200,
+        isBase64Encoded: false
+    };
+    // the form does not contain message or source fields
+    if (!message || !from) {
+        response.statusCode = 400;
+        response.body = JSON.stringify({
+            message: 'Malformed email form!',
+            errorType: "Malformed request"
+        }, null, 2);
+        return response;
+    }
+    const messageEscaped = htmlEscape(message);
+    const fromEscaped = htmlEscape(from);
+    const subjectEscaped = htmlEscape(subject);
 
     const params = {
         Destination: {
@@ -18,49 +43,26 @@ exports.handler = async event => {
         },
         Message: {
             Body: {
-                Text: {
+                Html: {
                     Charset: "UTF-8",
-                    Data: message
+                    Data: `<h4> Sender: ${fromEscaped}</h4><div>${messageEscaped}</div>`
                 }
             },
             Subject: {
                 Charset: "UTF-8",
-                Data: subject
+                Data: subjectEscaped || "Unknown",
             }
+
         },
-        Source: source,
+        Source: appSource,
     };
-
-
-    return await ses.sendEmail(params).promise().then(data => data).catch(err => {
-        console.log('function error'+err)
-        return { err: err, message: "sorry not possible"}
-
-    });
-
-    //     , function (err, data) {
-    //   if (err) console.log(err, err.stack); // an error occurred
-    //   else console.log(data);           // successful response
-    //   /*
-    //   data = {
-    //    MessageId: "EXAMPLE78603177f-7a5433e7-8edb-42ae-af10-f0181f34d6ee-000000"
-    //   }
-    //   */
-    // });
-
-
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify(
-    //         {
-    //             message: 'Go Serverless v1.0! Your function executed successfully!',
-    //             input: event,
-    //         },
-    //         null,
-    //         2
-    //     ),
-    // };
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return await ses.sendEmail(params).promise()
+        .then(data => {
+            response.body = JSON.stringify({success: 'Your message has been delivered', message: data}, null, 2);
+            return response;
+        })
+        .catch(err => {
+            response.body = JSON.stringify({error: 'Something happened. Try again later', message: err}, null, 2);
+            return response;
+        });
 };
