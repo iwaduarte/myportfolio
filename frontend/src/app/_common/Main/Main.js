5d969d584490f2c2b5c8322b1fb29c3e68c68e66
iwaduarte <iwaduarte@gmail.com>
2020-03-28 17:09:54 -0300

    Main.js to have a Switch for paths and to match every request

--- frontend/src/app/_common/Main/Main.js
+++ frontend/src/app/_common/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+// noinspection ES6CheckImport
+import {Route, Switch} from 'react-router-dom';
 import Bot from "../../bot/Bot";
 import Home from '../../home/Home'
 
 const Main = () => <>
-
-    <Route exact path="/" component={Bot}/>
-    <Route exact path="/y0ush0u1dn0th4v3gu3ss3d" component={Home}/>
+    <Switch>
+        <Route exact path="/y0ush0u1dn0th4v3gu3ss3d" component={Home}/>
+        <Route path="/" component={Bot}/>
+    </Switch>
 
 </>;
 
