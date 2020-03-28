1e0cac43d1bd46465fc51ca54068a30ac14a8431
iwaduarte <iwaduarte@gmail.com>
2020-03-27 23:53:01 -0300

    Added more videos to portfolio page

--- frontend/src/app/_config/videosList.js
+++ frontend/src/app/_config/videosList.js
@@ -11,7 +11,9 @@ const VideosList =
         8: 'https://www.youtube.com/embed/BKorP55Aqvg?start=1',
         9: 'https://www.youtube.com/embed/E91bGT9BjYk?start=1',
         10: 'https://www.youtube.com/embed/IesIsKMjB4Y?start=1',
-        11: 'https://www.youtube.com/embed/LOUf8Z0RQic?start=1',
+        11: 'https://www.youtube.com/embed/WfGMYdalClU?start=1',
+        12: 'https://www.youtube.com/embed/e9dZQelULDk?start=1',
+        13: 'https://www.youtube.com/embed/LOUf8Z0RQic?start=1',
     }
 
 
