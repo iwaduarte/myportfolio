7d2b2a2fb7d53e61e56748bbd32d82b4e3f29a15
iwaduarte <iwaduarte@gmail.com>
2020-03-28 17:10:25 -0300

    Use iterator in Article section

--- frontend/src/app/home/Articles.js
+++ frontend/src/app/home/Articles.js
@@ -1,18 +1,21 @@
 import React, {useState} from 'react';
 import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 
-const articles = []; //medium articles or personal blog development which is easier (less hassle)
+const articlesArr= [][Symbol.iterator](); //medium articles or personal blog development which is easier (less hassle)
 
-const handleClick = () => {
-    console.log('New article...');
-};
+
 const Articles = props => {
-    const [article, setArticle] = useState([]);
+    const [article, setArticle] = useState(articlesArr);
+
+    const handleClick = () => {
+        setArticle(articlesArr.next());
+        console.log('New article...');
+    };
 
     return < div className="article flex-container self-center ">
         <FaChevronLeft onClick={handleClick} size="50" color="rgb(108,103,134)" className="mx-1 pointer scale-hover"/>
-        {articles.length
-            ? articles.map(article => <div> {article}</div>)
+        {article.length
+            ? article.map(article => <div> {article}</div>)
             : <div className="self-center">Articles are being made... Wait a little my friend.
                 <span className="profile-emoji" role="img" aria-label='wink-emoji'> 😉 </span>
             </div>
