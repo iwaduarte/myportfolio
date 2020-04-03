548c3016925215fb74518a1c58fb2e37d89c338f
iwaduarte <iwaduarte@gmail.com>
2020-04-03 03:32:57 -0300

    Update email.js to be modular and simple
    - better unit testing

--- backend/email.js
+++ backend/email.js
@@ -1,27 +1,17 @@
 'use strict';
 //using only built-in libraries
 const AWS = require('aws-sdk');
-const querystring = require('querystring');
 const escapeHtml = require('./utils/escapeHtml');
+const deserialize = require('./utils/deserialize');
 
 const ses = new AWS.SES({apiVersion: "2010-12-01", region: "us-east-1"});
 const sendTo = process.env.SEND_TO;
 
 
-
-
 exports.handler = async event => {
-
-    const contentType = event.headers && (event.headers['content-type'] || event.headers['Content-Type']);
-    let body = {};
-
-    if (contentType && contentType.includes('x-www-form-urlencoded')) {
-        body = querystring.decode(event.body);
-
-    } else if (contentType && contentType.includes('json'))
-        body = JSON.parse(event.body);
-
+    const body = deserialize(event);
     const {message, appSource, from, subject} = body;
+
     const response = {
         statusCode: 200,
         headers: {
@@ -30,11 +20,11 @@ exports.handler = async event => {
         },
         isBase64Encoded: false
     };
-    // the form does not contain message or source fields
-    if (!message.trim() || !from.trim()) {
+    // the form does not contain message or source or appSource fields
+    if (!message.trim() || !from.trim() || !appSource.trim()) {
         response.statusCode = 400;
         response.body = JSON.stringify({
-            message: 'Malformed email form!',
+            message: `Malformed email form! Missing [message:${!!message}, from:${!!from}, appSource:${!!appSource}]`,
             errorType: "Malformed request"
         }, null, 2);
         return response;
