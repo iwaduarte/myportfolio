f27b54b5386a73580d59353ac45962a9729e23ee
iwaduarte <iwaduarte@gmail.com>
2020-04-03 03:33:36 -0300

    Create integration test

---
+++ backend/tests/integration.test.js
@@ -0,0 +1,14 @@
+
+const email = require('../email');
+jest.mock('aws-sdk');
+
+describe('fn: email.js - Testing aws lambda function', () => {
+
+    test('returns malformed url', () => {
+
+
+
+    });
+
+
+});
