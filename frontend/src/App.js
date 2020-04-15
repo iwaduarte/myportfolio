c62cfc56c272b1a6ce3d83a389857ad7c4d84899
iwaduarte <iwaduarte@gmail.com>
2020-04-15 18:30:06 -0300

    Implement user tracking with Google Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Main from "./app/_common/Main/Main";
 import Footer from "./app/_common/Main/Footer";
+import {createBrowserHistory} from 'history'
+import ReactGA from 'react-ga';
 
-const App = () => <>
-    <Main/>
-    <Footer/>
-</>;
+ReactGA.initialize('UA-163778059-1');
+const browserHistory = createBrowserHistory();
+browserHistory.listen((location, action) => {
+    ReactGA.pageview(location.pathname + location.search)
+});
+
+
+const App = () => {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }, []);
+
+    return <>
+        <Main/>
+        <Footer/>
+    </>
+
+};
 
 
 export default App;
