3ccd28fe189f332b4a7766d9be72bb311d0081fc
Iwá Duarte <iwaduarte@gmail.com>
2022-09-22 17:34:57 -0300

    feat: new App look and feel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,43 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import Main from './app/_common/Main/Main';
-import Footer from './app/_common/Main/Footer';
-import { createBrowserHistory } from 'history';
-import ReactGA from 'react-ga';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styles from './App.module.css';
+import SideBar from './app/_common/SideBar/SideBar';
+import Home from './app/home/Home';
+import About from './app/about/About';
+import Articles from './app/articles/Articles';
+import Projects from './app/projects/Projects';
 
-ReactGA.initialize('UA-163778059-1');
-const browserHistory = createBrowserHistory();
-browserHistory.listen((location, action) => {
-  ReactGA.pageview(location.pathname + location.search);
-});
+const NavBar = () => null;
+/* NavBar containing:
+ - linkedin
+ - github
+ - medium
+ - dark mode
 
-const App = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+ The NavBar has to have a X component that will close to the side. ( X => < )
+ The Nav will be sticky/fixed positioned on the right top corner of the screen
 
+ - language translation (to implement later in life )
+ - search (to implement later in life) :P
+ */
+
+const { home } = styles;
+
+function App() {
   return (
-    <>
-      <Main />
-      <Footer />
-    </>
+    <div className={home}>
+      <BrowserRouter>
+        <NavBar />
+        <SideBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-};
+}
 
 export default App;
