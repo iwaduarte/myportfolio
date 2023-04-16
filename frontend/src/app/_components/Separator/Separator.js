93325f39a71f93694c73c7e76e989a6db0303e1e
Iwá Duarte <iwaduarte@gmail.com>
2023-04-16 02:50:50 -0300

    feat: change dots to separator in order to separate cards

---
+++ frontend/src/app/_components/Separator/Separator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './Sepator.module.css';
+const { separator } = style;
+
+const Separator = () => <div className={separator}></div>;
+
+export default Separator;
