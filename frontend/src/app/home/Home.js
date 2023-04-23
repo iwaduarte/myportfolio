2b59c7f8ad7907d85c7223cfa6b8fb85304629bd
Iwá Duarte <iwaduarte@gmail.com>
2023-04-23 19:14:38 -0300

    feat: remove unnecessary import by using pseudo selectors

--- frontend/src/app/home/Home.js
+++ frontend/src/app/home/Home.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import styles from './Home.module.css';
-import styles2 from '../_components/Boxes/Boxes.module.css';
 import Boxes from '../_components/Boxes/Boxes';
 import Videos from './Videos';
 import NavBarMobile from '../_components/NavBarMobile/NavBarMobile';
 
 const { line, main } = styles;
-const { preload } = styles2;
 
 const Home = () => {
   const [modal, setModal] = useState(false);
@@ -15,7 +13,6 @@ const Home = () => {
     <div>
       <NavBarMobile />
       <div className={line} />
-      <div className={preload}></div>
       <div className={main}>
         <Boxes to="about" name="ABOUT ME" className="about" />
         <Boxes name="VIDEOS" className="videos" onClick={() => setModal(true)} />
