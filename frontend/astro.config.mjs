d9f1fcd6259235d631f0e0520d148a48f4d63486
iwaduarte <iwaduarte@gmail.com>
2023-09-14 23:49:46 -0300

    feat: add mdx and embed media markdown

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import mdx from '@astrojs/mdx';
+import fauxRemarkEmbedder from '@remark-embedder/core'
+import fauxOembedTransformer from '@remark-embedder/transformer-oembed'
+const remarkEmbedder = fauxRemarkEmbedder.default
+const oembedTransformer = fauxOembedTransformer.default
+
 
 // <https://astro.build/config>
 export default defineConfig({
-  integrations: [react()]
+  integrations: [mdx(
+    {
+      remarkPlugins:  [[remarkEmbedder, {transformers: [oembedTransformer]}]],
+    }
+  ), react()]
 });
